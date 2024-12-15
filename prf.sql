56e192448a40e6f0d84bd39a0ebeba2b815c2c33
Mikhail Korepanov <mikhail-korepanov@yandex.ru>
2024-12-15 13:54:08 +0400

    add some fields

--- prf.sql
+++ prf.sql
@@ -1,6 +1,10 @@
 CREATE TABLE SystemService.PRF(
+  operation_target_value Int16, 
+  operation_priority Int16,
+  file LowCardinality(String),
+  conf_name LowCardinality(String),
+  conf_version  LowCardinality(String),
   host LowCardinality(String),
-  operation_comment LowCardinality(String),
   ts DateTime,
   operation_name LowCardinality(String),
   operation_uid LowCardinality(String),
--- sql_inits/create_all.sql
+++ sql_inits/create_all.sql
@@ -514,8 +514,12 @@ ORDER BY (ts) TTL toDateTime(ts) + toIntervalDay(30);
 
 
 CREATE TABLE SystemService.PRF(
+  operation_target_value Int16, 
+  operation_priority Int16,
+  file LowCardinality(String),
+  conf_name LowCardinality(String),
+  conf_version  LowCardinality(String),
   host LowCardinality(String),
-  operation_comment LowCardinality(String),
   ts DateTime,
   operation_name LowCardinality(String),
   operation_uid LowCardinality(String),
